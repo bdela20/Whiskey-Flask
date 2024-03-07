f7440aa9e8d6c2c5b38452fc0e66b566af1da5b7
bdela20 <benjamindelarosa20@gmail.com>
2024-03-06 20:48:55 -0500

    second commit

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, render_template
 from helpers import token_required
-from models import db, User, Contact, contact_schema, contacts_schema
+from models import db, User, Whiskey, whiskey_schema, whiskeys_schema
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
@@ -10,58 +10,57 @@ def getdata():
 
 @api.route('/whiskeys', methods = ['POST'])
 @token_required
-def create_contact(current_user_token):
-    name = request.json['name']
-    email = request.json['email']
-    phone_number = request.json['phone_number']
-    address = request.json['address']
+def create_whiskey(current_user_token):
+    brand = request.json['brand']
+    litres = request.json['litres']
+    country = request.json['country']
+   
     user_token = current_user_token.token
 
     print(f'BIG TESTER: {current_user_token.token}')
 
-    contact = Contact(name, email, phone_number, address, user_token = user_token )
-
-    db.session.add(contact)
+    whiskey = Whiskey(brand, litres, country, user_token = user_token )
+    
+    db.session.add(whiskey)
     db.session.commit()
 
-    response = contact_schema.dump(contact)
+    response = whiskey_schema.dump(whiskey)
     return jsonify(response)
 
 
 @api.route('/whiskeys', methods=['GET'])
 @token_required
-def get_contact(current_user_token):   
+def get_whiskey(current_user_token):   
     a_user = current_user_token.token
-    contacts = Contact.query.filter_by(user_token=a_user).all()  # Corrected line
-    response = contacts_schema.dump(contacts)
+    whiskeys = Whiskey.query.filter_by(user_token=a_user).all()  # Corrected line
+    response = whiskeys_schema.dump(whiskeys)
     return jsonify(response)
 
 @api.route('/whiskeys/<id>', methods = ['GET'])
 @token_required
-def get_single_contact(current_user_token, id):
-    contact = Contact.query.get(id)
-    response = contact_schema.dump(contact)
+def get_single_whiskey(current_user_token, id):
+    whiskey = Whiskey.query.get(id)
+    response = whiskey_schema.dump(whiskey)
     return jsonify(response)
 
 @api.route('/whiskeys/<id>', methods = ['POST','PUT'])
 @token_required
-def update_contact(current_user_token,id):
-    contact = Contact.query.get(id) 
-    contact.name = request.json['name']
-    contact.email = request.json['email']
-    contact.phone_number = request.json['phone_number']
-    contact.address = request.json['address']
-    contact.user_token = current_user_token.token
+def update_whiskey(current_user_token,id):
+    whiskey = Whiskey.query.get(id) 
+    whiskey.brand = request.json['brand']
+    whiskey.litres = request.json['litres']
+    whiskey.country = request.json['country']
+    whiskey.user_token = current_user_token.token
 
     db.session.commit()
-    response = contact_schema.dump(contact)
+    response = whiskey_schema.dump(whiskey)
     return jsonify(response)
 
 @api.route('/whiskeys/<id>', methods = ['DELETE'])
 @token_required
-def delete_contact(current_user_token, id):
-    contact = Contact.query.get(id)
-    db.session.delete(contact)
+def delete_whiskey(current_user_token, id):
+    whiskey = Whiskey.query.get(id)
+    db.session.delete(whiskey)
     db.session.commit()
-    response = contact_schema.dump(contact)
+    response = whiskey_schema.dump(whiskey)
     return jsonify(response)
--- models.py
+++ models.py
@@ -49,31 +49,29 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f'User {self.email} has been added to the database'
     
-class Contact(db.Model):
+class Whiskey(db.Model):
     id =db.Column(db.String, primary_key=True)
-    name = db.Column(db.String(150), nullable = False)
-    email = db.Column(db.String(200))
-    phone_number = db.Column(db.String(20))
-    address = db.Column(db.String(200))
+    brand = db.Column(db.String(150), nullable = False)
+    litres = db.Column(db.String(200))
+    country = db.Column(db.String(20))
     user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable = False)
 
-    def __init__(self, name, email, phone_number, address, user_token, id=''):
+    def __init__(self, brand, litres,  country, user_token, id=''):
         self.id = self.set_id()
-        self.name = name
-        self.email = email
-        self.phone_number = phone_number
-        self.address = address
+        self.brand = brand
+        self.litres = litres
+        self.country = country
         self.user_token = user_token
 
     def __repr__(self):
-        return f'The following has been added to the phonebook: {self.name}'
+        return f'The following has been added to the Whiskeys: {self.brand}'
     
     def set_id(self):
         return (secrets.token_urlsafe())
     
-class ContactSchema(ma.Schema):
+class WhiskeySchema(ma.Schema):
     class Meta:
-        fields = ['id', 'name', 'email', 'phone_number', 'address']
+        fields = ['id', 'brand', 'litres', 'country']
 
-contact_schema = ContactSchema()
-contacts_schema =ContactSchema(many=True)
+whiskey_schema = WhiskeySchema()
+whiskeys_schema =WhiskeySchema(many=True)
